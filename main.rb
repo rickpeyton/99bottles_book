1c9b350e575db857891a175e8aaad5e565382298
Rick Peyton <peytorb@gmail.com>
2018-08-13 21:37:18 -0500

    Chapter 4 - Refactoring

--- main.rb
+++ main.rb
@@ -15,29 +15,12 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 1
-      <<~VERSE
-        1 bottle of beer on the wall,
-        1 bottle of beer.
-        Take it down and pass it around,
-        no more bottles of beer on the wall.
-      VERSE
-    when 0
-      <<~VERSE
-        No more bottles of beer on the wall,
-        no more bottles of beer.
-        Go to the store and buy some more,
-        99 bottles of beer on the wall.
-      VERSE
-    else
-      <<~VERSE
-        #{number} bottles of beer on the wall,
-        #{number} bottles of beer.
-        Take one down and pass it around,
-        #{number - 1} #{container(number - 1)} of beer on the wall.
-      VERSE
-    end
+    <<~VERSE
+      #{quantity(number).capitalize} #{container(number)} of beer on the wall,
+      #{quantity(number)} #{container(number)} of beer.
+      #{action(number)},
+      #{quantity(successor(number))} #{container(successor(number))} of beer on the wall.
+    VERSE
   end
 
   def container(number)
@@ -45,6 +28,38 @@ class Bottles
       "bottle"
     else
       "bottles"
+    end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
     end
   end
 end
